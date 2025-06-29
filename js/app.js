2d366c6edef094306a063b3a669c609992b81a10
rawan mohamed <rawanmohammed365@gmail.com>
2025-06-29 13:59:39 +0300

    login & logout

---
+++ js/app.js
@@ -0,0 +1,32 @@
+function showPage(pageId) {
+    const sections = document.querySelectorAll("section");
+    sections.forEach(section => {
+      section.style.display = "none";
+    });
+  
+    const target = document.getElementById(pageId);
+    if (target) {
+      target.style.display = "block";
+    }
+  }
+
+window.onload = function () {
+    const user = JSON.parse(localStorage.getItem("loggedInUser"));
+  
+    const welcome = document.getElementById("welcomeMsg");
+    const loginBtn = document.getElementById("loginNav");
+    const logoutBtn = document.getElementById("logoutNav");
+  
+    if (user) {
+      if (welcome) welcome.innerText = `${user.username}`;
+      if (loginBtn) loginBtn.style.display = "none";
+      if (logoutBtn) logoutBtn.style.display = "inline-block";
+    } else {
+      if (welcome) welcome.innerText = "home";
+      if (loginBtn) loginBtn.style.display = "inline-block";
+      if (logoutBtn) logoutBtn.style.display = "none";
+    }
+  
+    showPage("home"); 
+  };
+  
---
+++ js/auth.js
@@ -0,0 +1,50 @@
+// register
+function registerUser() {
+    const username = document.getElementById("regUsername").value;
+    const password = document.getElementById("regPassword").value;
+  
+    if (!username || !password) {
+      alert(" Please enter your username and password");
+      return;
+    }
+  
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    if (users.find(u => u.username === username)) {
+      alert("User name is already used");
+      return;
+    }
+  
+    users.push({ username, password });
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("You have registered successfully, you can log in now.");
+    showPage("login");
+  }
+  
+  // login
+  function loginUser() {
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+  
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    const user = users.find(u => u.username === username && u.password === password);
+  
+    if (!user) {
+      alert(" The login information is incorrect ");
+      return;
+    }
+  
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    alert("Logged in successfully");
+    showPage("home");
+  
+    const welcome = document.getElementById("welcomeMsg");
+    if (welcome) welcome.innerText = `${user.username}`;
+  }
+  
+  // logout
+  function logoutUser() {
+    localStorage.removeItem("loggedInUser");
+    alert("You are logged out");
+    showPage("login");
+  }
+  
